8d2e2714a68b6bee10bf37f6f2099eb5adced104
Alexander Morosow <alexander.morosow@gmail.com>
2020-09-12 12:46:17 +0200

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,9 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 import { Provider } from '@preact/prerender-data-provider';
-import Header from './header';
 
 // Code-splitting is automated for routes
 import Home from '../routes/home';
-import Blogs from '../routes/blogs';
-import Blog from '../routes/blog';
-import Contact from '../routes/contact';
-import ContactSuccess from '../routes/contact-success';
 import NotFoundPage from '../routes/notfound';
 
 export default class App extends Component {
@@ -25,13 +20,8 @@ export default class App extends Component {
 		return (
 			<Provider value={props}>
 				<div id="app">
-					<Header />
 					<Router onChange={this.handleRoute}>
 						<Home path="/" />
-						<Blogs path="/blogs/" />
-						<Blog path="/blog/:name" />
-						<Contact path="/contact/" />
-						<ContactSuccess path="/contact/success" />
 						<NotFoundPage type="404" default />
 					</Router>
 				</div>
